80d5272eb0c7f968dacdf14ad7c490b276272ea2
GuiFSs <devguisantos@gmail.com>
2020-08-18 17:45:56 -0300

    feat: ensure DbAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -8,11 +8,11 @@ export class DbAddAccount implements AddAccount {
 
   async add (accountData: DbAddAccount.Params): Promise<DbAddAccount.Model> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    await this.addAccountRepository.add({
+    const account = await this.addAccountRepository.add({
       ...accountData,
       password: hashedPassword
     })
-    return null
+    return account
   }
 }
 
