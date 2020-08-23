bececd9d6891b07f348464f4b2e84d725075e7d9
GuiFSs <devguisantos@gmail.com>
2020-08-23 00:20:02 -0300

    feat: add login route

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -9,7 +9,7 @@ import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt
 import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 import env from '../../config/env'
 
-export const makeLoginUpController = (): Controller => {
+export const makeLoginController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
