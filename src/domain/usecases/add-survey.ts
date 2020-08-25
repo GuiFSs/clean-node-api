ef9873db25417e4cf29496b778870b2f78e39c1e
GuiFSs <devguisantos@gmail.com>
2020-08-24 23:45:18 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,15 @@
+export interface AddSurvey {
+  add (data: AddSurvey.Params): Promise<void>
+}
+
+export namespace AddSurvey {
+  export interface Params {
+    question: string
+    answers: SurveyAnswers[]
+  }
+}
+
+export interface SurveyAnswers {
+  image: string
+  answer: string
+}
