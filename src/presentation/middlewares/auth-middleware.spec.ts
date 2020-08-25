e3766dd68994594cc51e8bd57ce32e108e1fd7af
GuiFSs <devguisantos@gmail.com>
2020-08-25 13:36:23 -0300

    test: ensure AuthMiddleware returns 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -54,4 +54,11 @@ describe('Auth Middleware', () => {
     await sut.handle(makeFakeHttpRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
+
+  test('Should return 403 if no LoadAccountByToken returns null', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockResolvedValueOnce(null)
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
 })
