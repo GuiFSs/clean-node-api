30eea9f8b9a2a7d3e101c6691dd35f6e8e68a936
GuiFSs <devguisantos@gmail.com>
2020-08-26 17:19:41 -0300

    test: ensure SurveyResultMongoRepository updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-respository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-respository.spec.ts
@@ -73,5 +73,26 @@ describe('SurveyResultMongo Repository', () => {
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toEqual(survey.answers[0].answer)
     })
+
+    test('Should update a survey result if its not new', async () => {
+      const { sut } = makeSut()
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const res = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toEqual(res.ops[0]._id)
+      expect(surveyResult.answer).toEqual(survey.answers[1].answer)
+    })
   })
 })
