aadabfbf58b22ca613411303cae4a28ef9e7ab24
GuiFSs <devguisantos@gmail.com>
2020-08-25 22:42:40 -0300

    feat: ensure add survey route returns 403 if no access token is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,22 @@
+import { HttpRequest, Middleware } from '../../presentation/protocols'
+import { Request, Response, NextFunction } from 'express'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const { statusCode, body } = await middleware.handle(httpRequest)
+    if (statusCode === 200) {
+      req = {
+        ...req,
+        ...body
+      }
+      next()
+    } else {
+      res.status(statusCode).json({
+        error: body.message
+      })
+    }
+  }
+}
