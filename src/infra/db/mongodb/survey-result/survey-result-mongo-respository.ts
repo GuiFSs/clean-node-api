5d6145e489b88f8f7881c5a7696029371b7e1350
GuiFSs <devguisantos@gmail.com>
2020-08-27 14:16:26 -0300

    refactor: use builder patterns to build the aggregation query

--- src/infra/db/mongodb/survey-result/survey-result-mongo-respository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-respository.ts
@@ -1,9 +1,8 @@
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResult } from '@/domain/usecases/survey-result/save-survey-result'
-import { MongoHelper } from '../helpers/mongo-helper'
 import { ObjectId } from 'mongodb'
-import { QueryBuilder } from '../helpers/query-builder'
+import { QueryBuilder, MongoHelper } from '../helpers'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (data: SaveSurveyResult.Params): Promise<SurveyResultModel> {
